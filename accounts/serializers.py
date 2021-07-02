fdec439e827872aa0efa04ae786ddf0b629e62c7
Yaroslav <hnflkmvjcm@gmail.com>
2021-07-02 13:30:18 +0300

    added JWT to registration

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,19 +1,24 @@
 from rest_framework import serializers
+from django.db import transaction
 from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
 
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
+    token = serializers.CharField(read_only=True)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
+        fields = ['username', 'email', 'password', 'token']
 
     def create(self, validated_data):
-
-        user = User.objects.create_user(
-            username=validated_data['username'],
-            email=validated_data['email'],
-            password=validated_data['password'],
-        )
+        with transaction.atomic():
+            user = User.objects.create_user(
+                username=validated_data['username'],
+                email=validated_data['email'],
+                password=validated_data['password'],
+            )
+            token = Token.objects.create(user=user)
+            user.token = token.key
         return user
