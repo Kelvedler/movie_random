5c962ba11ae0d1c8748b37db0c35669bc017572b
Yaroslav <hnflkmvjcm@gmail.com>
2021-07-15 13:13:15 +0300

    added review PUT api

--- movies/urls.py
+++ movies/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('genres/', views.GenreList.as_view()),
     path('genres/<int:pk>/', views.GenreDetail.as_view()),
     path('reviews/', views.ReviewCreate.as_view()),
+    path('reviews/<int:pk>/', views.ReviewDetail.as_view()),
     path('reviews/movie=<int:movie_id>/', views.MovieReviewList.as_view()),
     path('reviews/account=<int:account_id>/', views.AccountReviewList.as_view()),
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import Http404
 from rest_framework import generics, permissions, views, status
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
@@ -33,12 +34,37 @@ class ReviewCreate(views.APIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, format=None):
-        user = Token.objects.get(key=request.auth.key).user_id
-        request.data['user'] = user
+        user_id = Token.objects.get(key=request.auth.key).user_id
+        request.data['user'] = user_id
         serializer = ReviewSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ReviewDetail(views.APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self, pk):
+        try:
+            return Review.objects.get(pk=pk)
+        except Review.DoesNotExist:
+            raise Http404
+
+    def put(self, request, pk, format=None):
+        user_id = Token.objects.get(key=request.auth.key).user_id
+        review = self.get_object(pk)
+        if review.user_id != user_id:
+            return Response({"message": "prohibited from changing other users review"}, status=status.HTTP_400_BAD_REQUEST)
+        if 'movie' in request.data and review.movie_id != request.data['movie']:
+            return Response({"message": "prohibited from changing movie"}, status=status.HTTP_400_BAD_REQUEST)
+        request.data['user'] = review.user_id
+        request.data['movie'] = review.movie_id
+        serializer = ReviewSerializer(review, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
