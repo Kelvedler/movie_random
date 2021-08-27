03f4b527ddd788ceb2494629317b4b92643b3210
Yaroslav <hnflkmvjcm@gmail.com>
2021-08-28 02:50:25 +0300

    added allowed host

--- movie_random/settings.py
+++ movie_random/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'secret_key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['movie-random.herokuapp.com']
 
 
 # Application definition
